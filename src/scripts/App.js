22f1feeb702b64f0583fb72a101cd2a17fa45b5a
andrey1303110 <andrey1303110@gmail.com>
2022-09-09 19:28:03 +0300

    chagne directory for inex.js, test build with canvas add

---
+++ src/scripts/App.js
@@ -0,0 +1,10 @@
+import * as PIXI from "pixi.js";
+
+export class App {
+    run() {
+        this.app = new PIXI.Application({resizeTo: window});
+        console.log(this.app);
+
+        document.body.appendChild(this.app.view);
+    }
+}
