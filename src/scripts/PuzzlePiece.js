41f0fec4318f7fc15a0bcb70b499a4375d4884c7
andrey1303110 <andrey1303110@gmail.com>
2022-09-11 17:57:01 +0300

    add logic for dragging pieces

--- src/scripts/PuzzlePiece.js
+++ src/scripts/PuzzlePiece.js
@@ -8,5 +8,45 @@ export class PuzzlePiece {
         this.sprite.anchor.set(.5);
         this.sprite.width = document.body.clientWidth / 7;
         this.sprite.height = document.body.clientWidth / 7;
+        this.field = field;
+
+        this.setInteractive();
+    }
+
+    setInteractive() {
+        this.sprite.interactive = true;
+        this.sprite.on('pointerdown', this.onTouchStart, this);
+        this.sprite.on('pointerup', () => this.dragging = false, this);
+        this.sprite.on('pointermove', this.onTouchMove, this);
+    }
+
+    onTouchStart(event) {
+        this.touchPosition = {
+            x: event.data.global.x,
+            y: event.data.global.y,
+        };
+
+        this.dragging = true;
+    }
+
+    onTouchEnd() {
+
+    }
+
+    onTouchMove(event) {
+        if (!this.dragging) {
+            return;
+        }
+
+        const currentPosition = {
+            x: event.data.global.x,
+            y: event.data.global.y,
+        }
+
+        const offsetX = currentPosition.x - this.touchPosition.x;
+        const offsetY = currentPosition.y - this.touchPosition.y;
+
+        this.sprite.x = this.field.x + offsetX;
+        this.sprite.y = this.field.y + offsetY;
     }
 }
